90b36d0177486dce181dadfc8ebf30a2238c16b5
DESKTOP-9K2NI32\Liu Yiming <e0031565@u.nus.edu>
2018-02-09 00:04:27 +0800

    Split Address class into 4 classes.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,12 +8,24 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
-
+    public static final String EXAMPLE = "123, some street, #01-01, 123456";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should be in the format of"
+                                                                + "Block, Street, Unit, Postal Code";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+?(?=,),.+?(?=,),.+?(?=,),.+";
+    public static final String ADDRESS_SPLIT_REGEX = ",";
+    public static final String SEPARATOR = ", ";
+    public static final int BLOCK_INDEX = 0;
+    public static final int STREET_INDEX = 1;
+    public static final int UNIT_INDEX = 2;
+    public static final int POSTALCODE_INDEX = 3;
     public final String value;
+
+
     private boolean isPrivate;
+    private final Block block;
+    private final Street street;
+    private final Unit unit;
+    private final PostalCode postalCode;
 
     /**
      * Validates given address.
@@ -26,7 +38,13 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        String[] addressParameters = trimmedAddress.split(ADDRESS_SPLIT_REGEX);
+        this.block = new Block(addressParameters[BLOCK_INDEX], isPrivate);
+        this.street = new Street(addressParameters[STREET_INDEX], isPrivate);
+        this.unit = new Unit(addressParameters[UNIT_INDEX], isPrivate);
+        this.postalCode = new PostalCode(addressParameters[POSTALCODE_INDEX], isPrivate);
+        this.value = block.toString() + SEPARATOR + street.toString() + SEPARATOR + unit.toString()
+                        + SEPARATOR + postalCode.toString();
     }
 
     /**
